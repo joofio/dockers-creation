d86651c9ca890980720204e23af019d9a4aefec8
João Almeida <joaofilipe90@gmail.com>
2020-02-24 14:06:07 +0000

    Create install_packages.R

---
+++ h2o-r-slim/install_packages.R
@@ -0,0 +1,9 @@
+# The following two commands remove any previously installed H2O packages for R.
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+# Next, we download packages that H2O depends on.
+pkgs <- c("RCurl","jsonlite","h2o")
+for (pkg in pkgs) {
+if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+}
